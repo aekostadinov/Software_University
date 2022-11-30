4b08aee99e0ac3077c37dd0d7ae81f2bd1347664
aekostadinov <aemilov01@gmail.com>
2022-11-30 19:43:29 +0200

    02_Destination_Mapper.py

---
+++ Python Fundamentals/Final_Exams_Preparing/02_Destination_Mapper.py
@@ -0,0 +1,45 @@
+"""You will be given a string representing some places on the map. You have to filter only
+the valid ones. A valid location is:
+	•	Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+	•	After the first "=" or "/" there should be only letters (the first must be upper-case,
+	other letters could be upper or lower-case)
+	•	The letters must be at least 3
+Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the first two
+locations are valid.After you have matched all the valid locations, you have to calculate travel
+points. They are calculated by summing the lengths of all the valid destinations that you have
+found on the map. In the end, on the first line, print: "Destinations: {destinations joined by ', '}".
+On the second line, print "Travel Points: {travel_points}".
+
+Input / Constraints
+	•	You will receive a string representing the locations on the map
+	•	JavaScript: you will receive a single parameter: string
+
+Output
+	•	Print the messages described above"""
+
+
+
+
+import re
+
+pattern = r"(?<=(/|\=))[A-Z][a-zA-Z]{2,}(?=\1)"
+text = input()
+
+valid_destination = [destination.group() for destination in re.finditer(pattern,text)]
+travel_points = sum([len(destination) for destination in valid_destination])
+
+print(f"Destinations: {', '.join(valid_destination)}")
+print(f"Travel Points: {travel_points}")
+
+
+
+# import re
+#
+# pattern = r"(?<=(/|\=))(?P<DESTINATION>[A-Z][a-zA-Z]{2,})(?=\1)"
+# text = input()
+#
+# valid_destination = [destination['DESTINATION'] for destination in re.finditer(pattern,text)]
+# travel_points = sum([len(destination) for destination in valid_destination])
+#
+# print(f"Destinations: {', '.join(valid_destination)}")
+# print(f"Travel Points: {travel_points}")
