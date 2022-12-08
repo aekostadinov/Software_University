0408c9bbe87533faaa50bcffd14f0a6af6dfc2c6
aekostadinov <aemilov01@gmail.com>
2022-12-08 14:42:12 +0200

    Another sollution on task 05_Track_Tour

--- Python Advanced/Lists_as_Stacks_and_Queues - EXERCISE/05_Truck_Tour.py
+++ Python Advanced/Lists_as_Stacks_and_Queues - EXERCISE/05_Truck_Tour.py
@@ -50,3 +50,27 @@ for _ in range(number_of_pumps):
 print(current_index)
 
 
+
+
+
+
+# from collections import deque
+#
+# pumps_count = int(input())
+# pumps = deque()
+# for _ in range(pumps_count):
+#     pumps.append([int(x) for x in input().split()])
+#
+# for attempt in range(pumps_count):
+#     trunk = 0
+#     failed = False
+#     for petrol, distance in pumps:
+#         trunk = trunk + petrol - distance
+#         if trunk < 0:
+#             failed = True
+#             break
+#     if failed:
+#         pumps.append(pumps.popleft())
+#     else:
+#         print(attempt)
+#         break
