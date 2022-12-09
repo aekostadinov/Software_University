b9cca8b74f32dc4beb4087b8f3422ebb0ea38d1c
aekostadinov <aemilov01@gmail.com>
2022-12-09 23:40:20 +0200

    Create 03_Periodic_Table.py

---
+++ Python Advanced/Tuples_and_Sets - EXERCISES   /03_Periodic_Table.py
@@ -0,0 +1,12 @@
+"""Write a program that keeps all the unique chemical elements. On the first line, you will be given a number n -
+the count of input lines that you will receive. On the following n lines, you will be receiving chemical compounds
+separated by a single space. Your task is to print all the unique ones on separate lines (the order does not matter)"""
+
+count_of_inputs = int(input())
+sets_of_elements = set()
+for _ in range(count_of_inputs):
+    current_set = {element for element in input().split()}
+    sets_of_elements = current_set.union(sets_of_elements)
+for element in sets_of_elements:
+    print(element)
+
