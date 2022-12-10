f6b13bea00fd821308a6e6808227100b9e044979
aekostadinov <aemilov01@gmail.com>
2022-12-10 10:16:26 +0200

    Tuples_and_Sets - Tasks commit

---
+++ Python Advanced/Tuples_and_Sets - EXERCISES   /05_Longest_Intersection.py
@@ -0,0 +1,24 @@
+"""Write a program that finds the longest intersection. You will be given a number N. On each of the next N lines you
+will be given two ranges in the format: "{first_start},{first_end}-{second_start},{second_end}". You should find the
+intersection of these two ranges. The start and end numbers in the ranges are inclusive.
+Finally, you should find the longest intersection of all N intersections, print the numbers that are included and its
+length in the format: "Longest intersection is [{longest_intersection_numbers}] with length {length_longest_intersection}"
+
+Note: in each range, there will always be an intersection. If there are two equal intersections, print the first one."""
+
+
+count_of_lines = int(input())
+intersection_set = set()
+for _ in range(count_of_lines):
+    first_set_info, second_set_info = input().split("-")
+    first_start, first_end = (int(x) for x in first_set_info.split(','))
+    second_start, second_end = (int(y) for y in second_set_info.split(','))
+    first_set = {num for num in range(first_start,first_end+1)}
+    second_set = {num for num in range(second_start, second_end+1)}
+    current_intersection = first_set.intersection(second_set)
+    if len(current_intersection) > len(intersection_set):
+        intersection_set = current_intersection
+print(f"Longest intersection is {[number for number in intersection_set]} with length {len(intersection_set)}")
+
+
+
---
+++ Python Advanced/Tuples_and_Sets - EXERCISES   /06_Battle_Of_Names.py
@@ -0,0 +1,33 @@
+"""You will receive a number N. On the following N lines, you will be receiving names. You should sum the ASCII values
+of each letter in the name and integer divide it by the number of the current row (starting from 1). Save the result to
+a set of either odd or even numbers, depending on if the resulting number is odd or even. After that, sum the values of
+each set.
+· If the sums of the two sets are equal, print the union of the values, separated by ", ".
+· If the sum of the odd numbers is bigger than the sum of the even numbers, print the different values,
+separated by ", ".
+· If the sum of the even numbers is bigger than the sum of the odd numbers, print the symmetric-different values,
+separated by ", ".
+
+NOTE: On every operation, the starting set should be the odd set"""
+
+count_of_names = int(input())
+even_set = set()
+odd_set = set()
+for row in range(1,count_of_names+1):
+    name = input()
+    current_ascii_sum = 0
+    for ch in name:
+        current_ascii_sum += ord(ch)
+    current_ascii_sum = int(current_ascii_sum / row)
+    if current_ascii_sum % 2 == 0:
+        even_set.add(current_ascii_sum)
+    else:
+        odd_set.add(current_ascii_sum)
+if sum(even_set) == sum(odd_set):
+    result = even_set.union(odd_set)
+elif sum(even_set) > sum(odd_set):
+    result = even_set.symmetric_difference(odd_set)
+else:
+    result = odd_set.difference(even_set)
+print(*result, sep=', ')
+
