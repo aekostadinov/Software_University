9cbf0ae2807b00a5f6bca1de209575a436bb8b9a
aekostadinov <aemilov01@gmail.com>
2022-12-14 00:32:43 +0200

    Update 04_Sum_Matrix_Columns.py

--- Python Advanced/Multidimensional Lists - LAB/04_Sum_Matrix_Columns.py
+++ Python Advanced/Multidimensional Lists - LAB/04_Sum_Matrix_Columns.py
@@ -2,11 +2,22 @@
 On the first line, you will get matrix sizes in format "{rows}, {columns}". On the next rows, you will get
 elements for each column separated with a single space."""
 
+### Variant 1 ###
+# row, column = map(int, input().split(", "))
+# matrix = [[int(num) for num in input().split()] for _ in range(row)]
+# for column_index in range(column):
+#     current_sum = 0
+#     for row_index in range(row):
+#         current_sum += matrix[row_index][column_index]
+#     print(current_sum)
 
+### Variant 2 ###
 row, column = map(int, input().split(", "))
 matrix = [[int(num) for num in input().split()] for _ in range(row)]
-for column_index in range(column):
-    current_sum = 0
-    for row_index in range(row):
-        current_sum += matrix[row_index][column_index]
-    print(current_sum)
+column_sums = [0] * row
+for row_index in range(row):
+    for column_index in range(column):
+        value = matrix[row_index][column_index]
+        column_sums[column_index] += value
+print(column_sums)
+
