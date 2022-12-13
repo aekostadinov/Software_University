39c96b4ec79beb81c2ccae04089b67361db1e9fa
aekostadinov <aemilov01@gmail.com>
2022-12-14 01:20:04 +0200

    Update 05_Primary_Diagonal.py

--- Python Advanced/Multidimensional Lists - LAB/05_Primary_Diagonal.py
+++ Python Advanced/Multidimensional Lists - LAB/05_Primary_Diagonal.py
@@ -5,6 +5,5 @@ each column - N numbers, separated by a single space. """
 matrix_size = int(input())
 sum_of_primary_diagonal_numbers = 0
 matrix = [[int(num) for num in input().split()] for _ in range(matrix_size)]
-for index in range(matrix_size):
-    sum_of_primary_diagonal_numbers += matrix[index][index]
-print(sum_of_primary_diagonal_numbers)
+print(sum([matrix[i][i] for i in range(matrix_size)]))
+
