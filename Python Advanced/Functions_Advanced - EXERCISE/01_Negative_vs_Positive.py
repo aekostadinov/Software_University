555630ecdb283edd61fe3068392027b3cf707ef6
aekostadinov <aemilov01@gmail.com>
2022-12-27 06:21:18 +0200

    Update 01_Negative_vs_Positive.py

--- Python Advanced/Functions_Advanced - EXERCISE/01_Negative_vs_Positive.py
+++ Python Advanced/Functions_Advanced - EXERCISE/01_Negative_vs_Positive.py
@@ -9,6 +9,7 @@ from the positive. Find the total sum of the negatives and positives, and print 
 "The positives are stronger than the negatives"
 
 Note: you will not receive any zeroes in the input."""
+### Variant 1 ###
 def negative_positive(args):
     positive_sum = 0
     negative_sum = 0
@@ -23,3 +24,26 @@ def negative_positive(args):
         return f"{negative_sum}" + '\n' + f"{positive_sum}" + '\n' + "The positives are stronger than the negatives"
 sequence_nums = [int(num) for num in input().split()]
 print(negative_positive(sequence_nums))
+
+
+
+### Variant 2 ###
+def find_positives(all_numbers):
+    return sum([digit for digit in all_numbers if digit > 0])
+
+
+def find_negatives(all_numbers):
+    return sum([digit for digit in all_numbers if digit < 0])
+
+
+def check_biggest_number(positive, negative):
+    if abs(positive) > abs(negative):
+        return "The positives are stronger than the negatives"
+    return "The negatives are stronger than the positives"
+
+
+numbers = [int(x) for x in input().split() if x[-1].isdigit()]
+positives = find_positives(numbers)
+negatives = find_negatives(numbers)
+print(f"{negatives}\n{positives}")
+print(check_biggest_number(positives, negatives))
