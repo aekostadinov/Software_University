075088590942c1f0f12b2e32b6d5640c309f3c2c
aekostadinov <aemilov01@gmail.com>
2022-12-27 21:55:03 +0200

    Create 05_Greeting_by_Name.js

---
+++ JS Basics/First Steps In Coding - Lab/05_Greeting_by_Name.js
@@ -0,0 +1,12 @@
+// function greetingByName(input) {
+//     let name = input[0];
+//     let greeting = "Hello, " + name + "!"
+//     console.log(greeting);
+// }
+// Variant 2 
+function greetingByName(input) {
+    let name = input[0];
+    console.log(`Hello, ${name}!`);
+}
+
+greetingByName(['Petar']);
