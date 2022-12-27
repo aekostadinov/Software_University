d46cc6094ff6144584c6b4b1a72a48200756957d
aekostadinov <aemilov01@gmail.com>
2022-12-27 21:20:44 +0200

    JS Basics - Tasks Commit

---
+++ JS Basics/First Steps In Coding - Lab/02_Nums_1...10.js
@@ -0,0 +1,21 @@
+function numsFrom1to10() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    console.log(6);
+    console.log(7);
+    console.log(8);
+    console.log(9);
+    console.log(10);
+}
+numsFrom1to10();
+
+// Variant 2 
+
+for (let input=1;input<=10;input++){
+    console.log(input);
+}
+
+
