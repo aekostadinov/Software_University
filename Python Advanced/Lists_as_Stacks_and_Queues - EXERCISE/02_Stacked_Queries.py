eac7da43b4f67004806866612bdfb5e4dddebbfd
aekostadinov <aemilov01@gmail.com>
2023-01-16 22:33:34 +0200

    Update 02_Stacked_Queries.py

--- Python Advanced/Lists_as_Stacks_and_Queues - EXERCISE/02_Stacked_Queries.py
+++ Python Advanced/Lists_as_Stacks_and_Queues - EXERCISE/02_Stacked_Queries.py
@@ -29,3 +29,23 @@ for _ in range(number_of_commands):
             print(min(stack))
 print(', '.join(str(stack.pop()) for _ in range(len(stack))))
 
+### Variant 2 ###
+
+# from collections import deque
+#
+# numbers = deque()
+#
+# map_functions = {
+#     1: lambda x: numbers.append(x[1]),
+#     2: lambda x: numbers.pop() if numbers else None,
+#     3: lambda x: print(max(numbers)) if numbers else None,
+#     4: lambda x: print(max(numbers)) if numbers else None,
+# }
+#
+# for _ in range(int(input())):
+#     numbers_data = [int(x) for x in input().split()]
+#     map_functions[numbers_data[0]](numbers_data)
+#
+# numbers.reverse()
+#
+# print(*numbers, sep=", ")
