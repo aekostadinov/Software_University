037ed38d8d19782526fbd7a6c86c1a9a53dd616e
aekostadinov <aemilov01@gmail.com>
2023-01-21 18:13:57 +0200

    Updated 06_Symbol_in_Matrix

--- Python Advanced/Multidimensional Lists - LAB/06_Symbol_in_Matrix.py
+++ Python Advanced/Multidimensional Lists - LAB/06_Symbol_in_Matrix.py
@@ -4,22 +4,54 @@ Find the first occurrence of that symbol in the matrix and print its position in
 You should start searching from the top left. If there is no such symbol, print the message "{symbol} does not
 occur in the matrix"."""
 
-n = int(input())
-matrix = [input() for _ in range(n)]
-searched_symbol = input()
+# n = int(input())
+# matrix = [input() for _ in range(n)]
+# searched_symbol = input()
+#
+# is_found = False
+# for row in range(n):
+#     for column in range(n):
+#         if matrix[row][column] == searched_symbol:
+#             r,c = row, column
+#             is_found = True
+#             print((r,c))
+#             break
+#     if is_found:
+#         break
+# if not is_found:
+#     print(f"{searched_symbol} does not occur in the matrix")
+    
 
-is_found = False
-for row in range(n):
-    for column in range(n):
-        if matrix[row][column] == searched_symbol:
-            r,c = row, column
-            is_found = True
-            print((r,c))
-            break
-    if is_found:
-        break
-if not is_found:
-    print(f"{searched_symbol} does not occur in the matrix")
+
+
+### Variant 2 ###
+
+
+def read_matrix_func():
+    number_of_rows = int(input())
+    current_matrix = []
+    for row in range(number_of_rows):
+        row_data = list(input())
+        current_matrix.append(row_data)
+    return current_matrix
+
+def check_func_for_special_symbol(matrix, symbol):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            current_element = matrix[row][column]
+            if current_element == symbol:
+                return row,column
+
+def print_func(data,symbol):
+    if data:
+        print(data)
+    else:
+        print(f'{symbol} does not occur in the matrix')
+
+
+matrix = read_matrix_func()
+special_symbol = input()
+print_func(check_func_for_special_symbol(matrix,special_symbol), special_symbol)
 
 
 
