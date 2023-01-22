1011aaa9664e721b1ebdde95b2e308c2b82baad1
aekostadinov <aemilov01@gmail.com>
2023-01-22 15:10:10 +0200

    Update 05_Matrix_of_Palindromes.py

--- Python Advanced/Multidimensional_Lists - EXERCISES_1/05_Matrix_of_Palindromes.py
+++ Python Advanced/Multidimensional_Lists - EXERCISES_1/05_Matrix_of_Palindromes.py
@@ -6,11 +6,17 @@
 Input
     * The numbers r and c stay at the first line at the input in the format "{rows} {columns}"
     * r and c are integers in the range [1, 26]"""
+#
+# row, column = (int(ent) for ent in input().split())
+# matrix = [['' for _ in range(column)] for _ in range(row)]
+# for r in range(row):
+#     for c in range(column):
+#         current_string = chr(97 + r) + chr(97 + r + c) + chr(97 + r)
+#         matrix[r][c] += current_string
+#     print(' '.join(str for str in matrix[r]))
 
-row, column = (int(ent) for ent in input().split())
-matrix = [['' for _ in range(column)] for _ in range(row)]
-for r in range(row):
-    for c in range(column):
-        current_string = chr(97 + r) + chr(97 + r + c) + chr(97 + r)
-        matrix[r][c] += current_string
-    print(' '.join(str for str in matrix[r]))
+### Variant 2 ###
+
+row, column = [int(x) for x in input().split()]
+matrix = [[f"{chr(97+r)}{chr(97+r+c)}{chr(97+c)}" for c in range(column)] for r in range(row)]
+[print(*row) for row in matrix]
