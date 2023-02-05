ff48ba16d60c28dedc5e9087eeaab8b9977dfa12
aekostadinov <aemilov01@gmail.com>
2023-02-05 10:29:25 +0200

    Update 06_Function_Executor.py

--- Python Advanced/Functions_Advanced - EXERCISE/06_Function_Executor.py
+++ Python Advanced/Functions_Advanced - EXERCISE/06_Function_Executor.py
@@ -6,12 +6,30 @@ For more clarification, see the examples below.
 Submit only your function in the judge system."""
 
 
-def func_executor(*args):
-    output = ''
-    for element in args:
-        output += f"{element[0].__name__} - {element[0](*element[1])}" + '\n'
-    return output
+# def func_executor(*args):
+#     output = ''
+#     for element in args:
+#         output += f"{element[0].__name__} - {element[0](*element[1])}" + '\n'
+#     return output
+#
+#
+# def make_upper(*strings):
+#     result = tuple(s.upper() for s in strings)
+#     return result
+#
+# def make_lower(*strings):
+#     result = tuple(s.lower() for s in strings)
+#     return result
+#
+# print(func_executor(
+#     (make_upper, ("Python", "softUni")),
+#     (make_lower, ("PyThOn",)),
+# ))
 
+### Variant 2 ###
+
+def func_executor(*data):
+    return '\n'.join([f"{func.__name__} - {func(*args)}" for func, args in data])
 
 def make_upper(*strings):
     result = tuple(s.upper() for s in strings)
