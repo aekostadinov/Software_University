64a8f4de34dd3519860965caeee35a78ca712b2f
aekostadinov <aemilov01@gmail.com>
2023-02-25 20:22:17 +0200

    08_Pokemon_Battle Created

--- Python OOP/First Steps in OOP - EXERCISE/08_Pokemon_Battle/trainer.py
+++ Python OOP/First Steps in OOP - EXERCISE/08_Pokemon_Battle/trainer.py
@@ -1,35 +1,31 @@
 from project.pokemon import Pokemon
 
 class Trainer:
-    def __init__(self, name):
+    def __init__(self, name:str):
         self.name = name
         self.pokemons = []
 
-
-    def add_pokemon(self,pokemon):
+    def add_pokemon(self, pokemon:Pokemon):
         if pokemon in self.pokemons:
-            return "This pokemon is already caught"
-
+            return f"This pokemon is already caught"
         self.pokemons.append(pokemon)
+
         return f"Caught {pokemon.pokemon_details()}"
 
-
-    def release_pokemon(self,pokemon_name):
+    def release_pokemon(self,pokemon_name: str):
         try:
-            pokemon = next(filter(lambda p:p.name == pokemon_name, self.pokemons))
+            pokemon = next(filter(lambda p:p.name == pokemon_name,self.pokemons))
         except StopIteration:
-            return "Pokemon is not caught"
+            return f"Pokemon is not caught"
         self.pokemons.remove(pokemon)
-
         return f"You have released {pokemon_name}"
 
     def trainer_data(self):
-        result = []
-        result.append(f"Pokemon Trainer {self.name}")
-        result.append(f"Pokemon count {len(self.pokemons)}")
-        [result.append(f"- {p.pokemon_details()}") for p in self.pokemons]
+        pokemons_data = '\n'.join(f"- {p.pokemon_details()}" for p in self.pokemons)
+        return f"Pokemon Trainer {self.name}\n" +  \
+               f"Pokemon count {len(self.pokemons)}\n" + \
+               f"{pokemons_data}"
 
-        return '\n'.join(result)
 
 pokemon = Pokemon("Pikachu", 90)
 print(pokemon.pokemon_details())
