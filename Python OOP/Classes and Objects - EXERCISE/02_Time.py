fa89a6f270f8d946ec6b03899763c41e42eaaa3a
aekostadinov <aemilov01@gmail.com>
2023-02-28 09:56:35 +0200

    Update 02_Time.py

--- Python OOP/Classes and Objects - EXERCISE/02_Time.py
+++ Python OOP/Classes and Objects - EXERCISE/02_Time.py
@@ -7,22 +7,72 @@ should also create 3 additional instance methods:
 (use the get_time() method)"""
 
 
+# class Time:
+#     max_hours = 23
+#     max_minutes = 59
+#     max_seconds = 59
+#
+#     def __init__(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def set_time(self, hours, minutes, seconds):
+#         self.hours = hours
+#         self.minutes = minutes
+#         self.seconds = seconds
+#
+#     def get_time(self):
+#         if self.seconds > Time.max_seconds:
+#             self.seconds = 0
+#             self.minutes += 1
+#             if self.minutes > Time.max_minutes:
+#                 self.minutes = 0
+#                 self.hours += 1
+#                 if self.hours > Time.max_hours:
+#                     self.hours = 0
+#         if self.hours < 10:
+#             self.hours = f"0{self.hours}"
+#         if self.minutes < 10:
+#             self.minutes = f"0{self.minutes}"
+#         if self.seconds < 10:
+#             self.seconds = f"0{self.seconds}"
+#         return f"{self.hours}:{self.minutes}:{self.seconds}"
+#
+#     def next_second(self):
+#         self.seconds += 1
+#         return Time.get_time(self)
+
+
+# time = Time(00, 00, 00)
+# print(time.next_second())
+
+### VARIANT 2 ###
+
 class Time:
     max_hours = 23
     max_minutes = 59
     max_seconds = 59
 
-    def __init__(self, hours, minutes, seconds):
+    def __init__(self, hours: int, minutes: int, seconds: int):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
 
     def set_time(self, hours, minutes, seconds):
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
+       self.hours = hours
+       self.minutes = minutes
+       self.seconds = seconds
 
     def get_time(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self):
+        self.seconds += 1
+        self.update_valid_time()
+        return self.get_time()
+
+    def update_valid_time(self):
         if self.seconds > Time.max_seconds:
             self.seconds = 0
             self.minutes += 1
@@ -31,18 +81,3 @@ class Time:
                 self.hours += 1
                 if self.hours > Time.max_hours:
                     self.hours = 0
-        if self.hours < 10:
-            self.hours = f"0{self.hours}"
-        if self.minutes < 10:
-            self.minutes = f"0{self.minutes}"
-        if self.seconds < 10:
-            self.seconds = f"0{self.seconds}"
-        return f"{self.hours}:{self.minutes}:{self.seconds}"
-
-    def next_second(self):
-        self.seconds += 1
-        return Time.get_time(self)
-
-
-# time = Time(00, 00, 00)
-# print(time.next_second())
