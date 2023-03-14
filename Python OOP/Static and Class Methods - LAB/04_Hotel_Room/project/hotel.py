f34e6a23e335f7b21341ac133d0fbe14d65e3948
aekostadinov <aemilov01@gmail.com>
2023-03-14 12:28:26 +0200

    04_Hotel_Room Created

---
+++ Python OOP/Static and Class Methods - LAB/04_Hotel_Room/project/hotel.py
@@ -0,0 +1,59 @@
+from typing import List
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name: str):
+        self.name = name
+        self.rooms: List[Room] = []
+        self.guests: int = 0
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number: int, people: int):
+        room = next(filter(lambda r: r.number == room_number, self.rooms))
+        result = room.take_room(people)
+
+        if result:
+            return result
+        self.guests += people
+
+    def free_room(self, room_number: int):
+        room = next(filter(lambda r: r.number == room_number, self.rooms))
+        guests = room.guests
+        result = room.free_room()
+        if result:
+            return result
+        self.guests -= guests
+
+    def status(self):
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join(str(r.number) for r in self.rooms if not r.is_taken)}\n" \
+               f"Taken rooms: {', '.join(str(r.number) for r in self.rooms if r.is_taken)}"
+
+
+
+# from project.hotel import Hotel
+# from project.room import Room
+#
+# hotel = Hotel.from_stars(5)
+#
+# first_room = Room(1, 3)
+# second_room = Room(2, 2)
+# third_room = Room(3, 1)
+#
+# hotel.add_room(first_room)
+# hotel.add_room(second_room)
+# hotel.add_room(third_room)
+#
+# hotel.take_room(1, 4)
+# hotel.take_room(1, 2)
+# hotel.take_room(3, 1)
+# hotel.take_room(3, 1)
+#
+# print(hotel.status())
